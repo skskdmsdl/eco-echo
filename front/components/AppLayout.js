ede87fcd987e108be0897e3d793ec74a0540e2a2
skskdmsdl <skskdmsdl@gmail.com>
2021-05-05 15:47:34 +0900

    파일 수정

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { Menu } from 'antd';
 
+import SearchForm from './SearchForm';
 import LoginForm from './LoginForm';
 
 const AppLayout = ({ children }) => {
@@ -13,11 +14,12 @@ const AppLayout = ({ children }) => {
                     <Link href='/'><a>쇼핑몰</a></Link>
                 </Menu.Item>
                 <Menu.Item>
-                    <Link href='/'><a>상품</a></Link>
+                    <Link href='/item'><a>상품</a></Link>
                 </Menu.Item>
                 <Menu.Item>
                     <Link href='/'><a>더보기</a></Link>
                 </Menu.Item>
+                <SearchForm />
                 <LoginForm />
             </Menu>
             {children}
--- front/pages/item.js
+++ front/pages/item.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import AppLayout from '../components/AppLayout';
+import ItemList from '../components/ItemList';
 
 const Item = () => {
     return (
         <AppLayout>
-
+             <ItemList />
         </AppLayout>
     );
 };
