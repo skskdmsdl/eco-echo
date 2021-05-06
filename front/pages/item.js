1521f9a36d8c55d7fe81e1f8dde26899826975fa
haeunji04 <ppippu12@gmail.com>
2021-05-06 14:59:27 +0900

    충돌 수정

--- front/pages/item.js
+++ front/pages/item.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
 import AppLayout from '../components/AppLayout';
-<<<<<<< HEAD
 import ItemList from "../components/ItemList";
 
 
-=======
-import ItemList from '../components/ItemList';
-
->>>>>>> 0dec7c93d00e7d250fe9ef99edd8974a6a6bbc22
 const Item = () => {
     return (
         <AppLayout>
