dfb502b3124bc1c3318e7f7f79a8a3f74679afb3
skskdmsdl <skskdmsdl@gmail.com>
2021-05-07 23:19:26 +0900

    회사소개 페이지 경로 설정

---
+++ front/pages/company.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import AppLayout from '../components/AppLayout';
+import ItemList from "../components/more/company.js";
+
+
+const Item = () => {
+    return (
+        <AppLayout>
+            <ItemList />
+        </AppLayout>
+    );
+};
+
+export default Item;
