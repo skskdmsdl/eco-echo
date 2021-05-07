7a515fd262b31344b9b61bd933fa8fae367e7fe0
haeunji04 <ppippu12@gmail.com>
2021-05-07 23:39:57 +0900

    최근 본 샵 추가 및 연결, selectCategory 컴포넌트 분리

---
+++ front/pages/currentShop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout'
+import CurrentShopList from '../components/CurrentShopList'
+
+const CurrentShop = () => {
+
+    return (
+        <AppLayout>
+            <CurrentShopList />
+        </AppLayout>
+    );
+}
+
+export default CurrentShop;
