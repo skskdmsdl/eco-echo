f1638cd8d9cd2f464631a7f52f5a29c1296e2b01
haeunji04 <ppippu12@gmail.com>
2021-05-14 22:49:05 +0900

    검색 조회 페이지 추가

---
+++ front/pages/search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+import SearchResult from '../components/SearchResult';
+
+const Search = () => {
+
+    return(
+        <AppLayout>
+            <SearchResult />
+        </AppLayout>
+    );
+
+};
+
+export default Search;
