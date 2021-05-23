cf548a13fed25b2f0a2f30cefcccd3ece228a140
haeunji04 <ppippu12@gmail.com>
2021-05-23 23:33:11 +0900

    컴포넌트 정리, 스타일 분리

--- front/pages/more/currentShop.js
+++ front/pages/more/currentShop.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import AppLayout from '../../components/AppLayout'
-import styled from 'styled-components';
 import { Button, Row, Divider } from 'antd';
 import OneShop from '../../components/OneShop'
 import { Title, AppDiv, ButtonWrapper, ButtonStyle } from './styles';
