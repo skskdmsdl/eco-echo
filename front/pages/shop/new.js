6ef5baf404dc0278c08cc65de879958d9df67372
skskdmsdl <skskdmsdl@gmail.com>
2021-05-24 22:50:20 +0900

    기업 회원가입 및 스토어 등록 페이지

---
+++ front/pages/shop/new.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { Form, Input, Button, Upload } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+
+import AppLayout from '../../components/AppLayout';
+
+const NewItem = () => {
+  const [form] = Form.useForm();
+  const [formLayout, setFormLayout] = useState('horizontal');
+
+  const onFormLayoutChange = ({ layout }) => {
+    setFormLayout(layout);
+  };
+
+  const formItemLayout =
+    formLayout === 'horizontal'
+      ? {
+          labelCol: {
+            span: 4,
+          },
+          wrapperCol: {
+            span: 14,
+          },
+        }
+      : null;
+  const buttonItemLayout =
+    formLayout === 'horizontal'
+      ? {
+          wrapperCol: {
+            span: 14,
+            offset: 4,
+          },
+        }
+      : null;
+    return (
+      <AppLayout>
+        <Form
+        style={{ padding: '150px' }}
+        {...formItemLayout}
+        layout={formLayout}
+        form={form}
+        initialValues={{ layout: formLayout }}
+        onValuesChange={onFormLayoutChange}
+      >
+        {/* <Form.Item label="Form Layout" name="layout">
+          <Radio.Group value={formLayout}>
+            <Radio.Button value="horizontal">Horizontal</Radio.Button>
+            <Radio.Button value="vertical">Vertical</Radio.Button>
+            <Radio.Button value="inline">Inline</Radio.Button>
+          </Radio.Group>
+        </Form.Item> */}
+         <Form.Item label="아이디">
+          <Input placeholder="input placeholder" />
+        </Form.Item>
+        <Form.Item label="비밀번호">
+          <Input placeholder="input placeholder" />
+        </Form.Item>
+        <Form.Item label="비밀번호 확인">
+          <Input placeholder="input placeholder" />
+        </Form.Item>
+        <Form.Item label="쇼핑몰명">
+          <Input placeholder="input placeholder" />
+        </Form.Item>
+        <Form.Item label="쇼핑몰 링크">
+          <Input placeholder="input placeholder" />
+        </Form.Item>
+        <Form.Item label="쇼핑몰 카테고리">
+          <Input placeholder="input placeholder" />
+        </Form.Item>
+        <div style={{margin:"0 500px 40px 195px"}}>
+          <Upload
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            listType="picture"
+            className="upload-list-inline"
+          >
+            <Button icon={<UploadOutlined />}>Upload</Button>
+          </Upload>
+        </div>
+        <Form.Item {...buttonItemLayout}>
+          <Button>상품등록</Button>
+        </Form.Item>
+      </Form>
+      </AppLayout>
+    );
+};
+
+export default NewItem;
