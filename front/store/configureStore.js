20598581bc0a3d1bab6e37785afec11c8b1ee27d
skskdmsdl <skskdmsdl@gmail.com>
2021-06-02 22:26:01 +0900

    스토어 추가

---
+++ front/store/configureStore.js
@@ -0,0 +1,34 @@
+import { createWrapper } from 'next-redux-wrapper';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+
+import reducer from '../reducers';
+import rootSaga from '../sagas';
+
+// action을 실행하기 전에 console.log를 찍어주는 middleware
+// redux-devtools를 대체 할 수 있음
+// const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
+//   console.log(action);
+//   return next(action);
+// };
+
+const configureStore = (context) => {
+  console.log(context);
+  const sagaMiddleware = createSagaMiddleware(); // saga에서 추가된 부분
+  const middlewares = [sagaMiddleware];
+  const enhancer = process.env.NODE_ENV === 'production'
+    ? compose(applyMiddleware(...middlewares))
+    : composeWithDevTools(
+      applyMiddleware(...middlewares),
+    );
+  const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga); // saga에서 추가된 부분
+  return store;
+};
+
+const wrapper = createWrapper(configureStore, {
+  debug: process.env.NODE_ENV === 'development',
+});
+
+export default wrapper;
