ab8850404bbc8760d6d49e4afb6b31da2896d38c
skskdmsdl <skskdmsdl@gmail.com>
2021-06-16 21:33:22 +0900

    콘솔 출력 삭제

--- front/reducers/index.js
+++ front/reducers/index.js
@@ -6,7 +6,6 @@ import user from './user';
 const rootReducer = (state, action) => {
   switch (action.type) {
     case HYDRATE:
-      console.log('HYDRATE', action);
       return action.payload;
     default: {
       const combinedReducer = combineReducers({
