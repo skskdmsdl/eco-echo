c5b841497e452ab7da2854d39aceb6cca5745821
skskdmsdl <skskdmsdl@gmail.com>
2021-06-17 23:13:10 +0900

    user 리듀서 추가

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -11,3 +11,52 @@ export const initialState = {
     me: null,
     userInfo: null,
   };
+
+  const reducer = (state = initialState, action) => produce(state, (draft) => {
+    switch (action.type) {
+      case LOG_IN_REQUEST: // 3개 다 초기화
+        draft.logInLoading = true;
+        draft.logInError = null;
+        draft.logInDone = false;
+        break;
+      case LOG_IN_SUCCESS: // loading 끄고 Done true
+        draft.logInLoading = false;
+        draft.me = action.data;
+        draft.logInDone = true;
+        break;
+      case LOG_IN_FAILURE: // loading 끄고 error 넣어주기
+        draft.logInLoading = false;
+        draft.logInError = action.error;
+        break;
+      case LOG_OUT_REQUEST:
+        draft.logOutLoading = true;
+        draft.logOutError = null;
+        draft.logOutDone = false;
+        break;
+      case LOG_OUT_SUCCESS:
+        draft.logOutLoading = false;
+        draft.logOutDone = true;
+        draft.me = null;
+        break;
+      case LOG_OUT_FAILURE:
+        draft.logOutLoading = false;
+        draft.logOutError = action.error;
+        break;
+      case SIGN_UP_REQUEST:
+        draft.signUpLoading = true;
+        draft.signUpError = null;
+        draft.signUpDone = false;
+        break;
+      case SIGN_UP_SUCCESS:
+        draft.signUpLoading = false;
+        draft.signUpDone = true;
+        break;
+      case SIGN_UP_FAILURE:
+        draft.signUpLoading = false;
+        draft.signUpError = action.error;
+        break;
+    
+    }
+  });
+  
+  export default reducer;
