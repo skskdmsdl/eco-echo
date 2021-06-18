a5e75e2b5cd3dc1aa4fedb65cf4d14063746eddc
skskdmsdl <skskdmsdl@gmail.com>
2021-06-18 20:53:37 +0900

    리듀서 추가

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -12,6 +12,32 @@ export const initialState = {
     userInfo: null,
   };
 
+export const LOAD_USER_REQUEST = 'LOAD_USER_REQUEST';
+export const LOAD_USER_SUCCESS = 'LOAD_USER_SUCCESS';
+export const LOAD_USER_FAILURE = 'LOAD_USER_FAILURE';
+
+export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
+
+export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
+export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
+export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
+
+export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
+
+
+export const loginRequestAction = (data) => ({
+  type: LOG_IN_REQUEST,
+  data,
+});
+
+export const logoutRequestAction = () => ({
+  type: LOG_OUT_REQUEST,
+});
+
   const reducer = (state = initialState, action) => produce(state, (draft) => {
     switch (action.type) {
       case LOG_IN_REQUEST: // 3개 다 초기화
