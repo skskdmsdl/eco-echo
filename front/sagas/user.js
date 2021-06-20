f63218867ac408dbe78b9d292667069b063cca46
skskdmsdl <skskdmsdl@gmail.com>
2021-06-20 21:15:33 +0900

    saga 추가

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -15,6 +15,19 @@ import {
 
 
 
+  
+function* watchLogIn() {
+  yield takeLatest(LOG_IN_REQUEST, logIn); // thunk의 비동기 actionCreator
+}
+
+function* watchLogOut() {
+  yield takeLatest(LOG_OUT_REQUEST, logOut);
+}
+
+function* watchSignUp() {
+  yield takeLatest(SIGN_UP_REQUEST, signUp);
+}
+
 export default function* userSaga() {
   yield all([
     fork(watchChangeNickname),
